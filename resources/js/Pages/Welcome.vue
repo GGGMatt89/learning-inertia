d6813306f78b4473477cc26ae9122de897c65cdb
GGGMatt89 <matt.fntn89@gmail.com>
2024-09-23 21:15:28 +0200

    Commit after lesson 2

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+    name: String,
+    frameworks: Array
+})
+</script>
+
+<template>
+    <h1>Hello, {{ name }}</h1>
+
+    <p>We will use the following:</p>
+    <ul>
+        <li v-for="framework in frameworks" v-text="framework"></li>
+    </ul>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view ('welcome');
+    return Inertia::render('Welcome', [
+        'name' => 'Mattia Fontana',
+        'frameworks' => [
+            'Laravel', 'Vue', 'Inertia'
+
+        ]
+    ]);
 });
